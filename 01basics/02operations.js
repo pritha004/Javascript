f8016e2cfd61da8643afc9461ee555f1904f41ba
pritha004 <115443887+pritha004@users.noreply.github.com>
2023-12-04 06:58:23 +0000

    Operations and Prefix Postfix

---
+++ 01basics/02operations.js
@@ -0,0 +1,18 @@
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+console.log(true); //true
+console.log(+true);//1
+console.log(+"");//0
+
+//Prefix and Postfix
+
+let a1=3;
+let b1=++a1;
+console.log(a1,b1);
+
+let a=3;
+let b=a++;
+console.log(a,b);
