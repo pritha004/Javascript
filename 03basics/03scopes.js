5f92392cf37494821ac211455c484fd0c97b3d70
pritha004 <115443887+pritha004@users.noreply.github.com>
2023-12-12 10:23:29 +0000

    Block scopes and hoisting

--- 03basics/03scopes.js
+++ 03basics/03scopes.js
@@ -1,7 +1,40 @@
-let a=10;
-const b=20;
-var c=30;
 
-console.log(a);
-console.log(b);
+var c=300; //Global scope-> var should not be used
+
+if(true){ //Block scope
+    let a=10;
+    const b=20;
+    var c=30; 
+}
+
+// console.log(a);
+// console.log(b);
 console.log(c);
+
+/*------------------------------------------ */
+
+function one(){
+    const username="alex";
+
+    function two(){
+        const website="youtube";
+        console.log(username);
+    }
+    // console.log(website); //gives error as accessed outside function two
+    two()
+}
+
+one()
+
+/*----------------------HOISTING-------------------- */
+
+console.log(addone(5)) //gives output
+function addone(num){
+    return num+1;
+}
+
+
+console.log(addTwo(5)) //cant call before function declaration in case of function expression->throws error
+const addTwo=function(num){ //function expression
+    return num+2
+}
