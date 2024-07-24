11b33128d7c45e19191855a6171083214cb4cc9b
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-07-24 10:35:01 +0000

    Error Handling

--- 30daysJS/D12_ErrorHandling.js
+++ 30daysJS/D12_ErrorHandling.js
@@ -19,7 +19,7 @@ const func2=(num1,num2)=>{
         console.error(error);
     }
 }
-func2(12,0);
+//func2(12,0);
 
 // Finally Block
 
@@ -44,3 +44,74 @@ class CustomError extends Error {
     }
   }
   
+function riskyOperation() {
+    try {
+        const errorCondition = true;
+  
+        if (errorCondition) {
+        throw new CustomError("Something went wrong in the risky operation.");
+        }
+        return "Operation completed successfully.";
+    } catch (error) {
+        if (error instanceof CustomError) {
+          console.error(`Custom Error caught: ${error.message}`);
+        } else {
+          console.error(`Unexpected error caught: ${error.message}`);
+        }
+      }
+}
+//riskyOperation();
+  
+function userValidate(val){
+    if(val && val!==""){
+        return val;
+    }else{
+        throw new CustomError("Value is not present.")
+    }
+}
+//userValidate("");
+
+// Error Handling in Promises
+
+const promise=new Promise((res,rej)=>{
+    const random=Math.floor(Math.random()*2);
+    if(random===0){
+        res("Resolved")
+    }else{
+        rej("Rejected");
+    }
+})
+
+// promise.then((val)=>{
+//     console.log(val);
+// }).catch((val)=>{
+//     console.log("error", val);
+// })
+
+const func=async()=>{
+    try {
+        const val=await promise;
+        console.log(val);
+    } catch (error) {
+        console.log(error);
+    }
+}
+func();
+
+// Error Handling in Fetch
+
+fetch('https://jsonplaceholder.typicode.co/posts/1')
+  .then((response) => response.json())
+  .then((json) => console.log(json))
+  .catch((error)=>console.log(error))
+
+const getPost=async()=>{
+    try {
+        const response= await fetch('https://jsonplaceholder.typicode.co/posts/1');
+        const data= await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+  }
+getPost();
